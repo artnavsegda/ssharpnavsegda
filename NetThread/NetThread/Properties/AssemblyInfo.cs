8e37392c594136c69fbd9a77d5094c175e245f65
Art Navsegda <artnavsegda@gmail.com>
2019-12-10 16:55:16 +0300

    net thread

---
+++ NetThread/NetThread/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NetThread")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetThread")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
