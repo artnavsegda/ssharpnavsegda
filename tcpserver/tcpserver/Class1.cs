8e0dcf5a572bddeb0f10b47e4113c1a7e46a4355
Art Navsegda <artnavsegda@gmail.com>
2019-12-10 17:47:16 +0300

    tcpserver

---
+++ tcpserver/tcpserver/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using Crestron.SimplSharp.CrestronSockets;
+
+namespace tcpserver
+{
+    public class MyTCPServer
+    {
+        static TCPServer server;
+        /// <summary>
+        /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
+        /// use an Initialize method
+        /// </summary>
+        public MyTCPServer()
+        {
+            //constructor
+        }
+
+        public static void MyFunction(string myString)
+        {
+            //sample function
+            server = new TCPServer(9999, 100);
+        }
+    }
+}
---
+++ tcpserver/tcpserver/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("tcpserver")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tcpserver")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
