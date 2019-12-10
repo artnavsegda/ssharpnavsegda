8dd3e7eed27bd1a6376a764edb2e83f10b8e1ee6
Art Navsegda <artnavsegda@gmail.com>
2019-12-10 18:52:11 +0300

    socket

--- tcpserver/tcpserver/Class1.cs
+++ tcpserver/tcpserver/Class1.cs
@@ -7,7 +7,7 @@ namespace tcpserver
 {
     public class MyTCPServer
     {
-        private TCPServer server;
+        public TCPServer server;
         /// <summary>
         /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
         /// use an Initialize method
@@ -27,9 +27,30 @@ namespace tcpserver
             //init server
             server = new TCPServer(9999, 100);
             server.SocketStatusChange += new TCPServerSocketStatusChangeEventHandler(ServerSocketStatusChanged);
+            server.WaitForConnectionAsync(ServerConnectedCallback);
         }
 
-        void ServerSocketStatusChanged(TCPServer server, uint clientIndex, SocketStatus status)
+        public void ServerSocketStatusChanged(TCPServer server, uint clientIndex, SocketStatus status)
+        {
+        }
+
+        public void ServerConnectedCallback(TCPServer server, uint clientIndex)
+        {
+            server.ReceiveDataAsync(clientIndex, Server_RecieveDataCallBack);
+        }
+
+        public void Server_RecieveDataCallBack(TCPServer server, uint clientIndex, int numberOfBytesReceived)
+        {
+            byte[] recvd_bytes = new byte[numberOfBytesReceived];
+            Array.Copy(server.GetIncomingDataBufferForSpecificClient(clientIndex), recvd_bytes, numberOfBytesReceived);
+            string recvd_msg = ASCIIEncoding.ASCII.GetString(recvd_bytes, 0, numberOfBytesReceived);
+            CrestronConsole.PrintLine("Client " + clientIndex + " says: " + recvd_msg + "\r\nEchoing back to client " + clientIndex + "...");
+            server.SendDataAsync(clientIndex, recvd_bytes, recvd_bytes.Length, ServerDataSentCallback);
+            server.ReceiveDataAsync(clientIndex, Server_RecieveDataCallBack);
+            CrestronConsole.PrintLine("---------- end of message ----------");
+        }
+
+        public void ServerDataSentCallback(TCPServer server, uint clientIndex, int bytesSent)
         {
         }
     }
