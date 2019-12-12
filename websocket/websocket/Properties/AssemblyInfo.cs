c5f1f4f8df8a3ebcf1663c1851491c6fc8fa6517
Art Navsegda <artnavsegda@gmail.com>
2019-12-12 14:43:28 +0300

    websocket

---
+++ websocket/websocket/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("websocket")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("websocket")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
