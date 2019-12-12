6ee0fb4d09a45f80a01676a2ad17f1a15e428212
Art Navsegda <artnavsegda@gmail.com>
2019-12-12 17:24:37 +0300

    l

---
+++ websocket2/websocket2/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("websocket2")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("websocket2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
