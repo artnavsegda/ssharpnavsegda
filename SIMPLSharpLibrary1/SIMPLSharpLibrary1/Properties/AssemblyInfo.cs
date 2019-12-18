7563b35f3dc5ac793efa32867e5031467b8eb91d
Art Navsegda <artnavsegda@gmail.com>
2019-12-18 16:06:01 +0300

    sandbox

---
+++ SIMPLSharpLibrary1/SIMPLSharpLibrary1/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SIMPLSharpLibrary1")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SIMPLSharpLibrary1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
