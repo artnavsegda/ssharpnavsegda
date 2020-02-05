c0d989b03d1a2d72a8475419e659336a228e03b0
Art Navsegda <artnavsegda@gmail.com>
2020-02-05 19:08:30 +0300

    vlb

--- SIMPLSharpLibrary1/SIMPLSharpLibrary1/Class1.cs
+++ SIMPLSharpLibrary1/SIMPLSharpLibrary1/Class1.cs
@@ -6,13 +6,20 @@ namespace SIMPLSharpLibrary1
 {
     public class Class1
     {
-
+        int ID = 12;
+        public delegate void CallbackHandler(SimplSharpString data);
+        public CallbackHandler CallbackEvent { set; get; }
         /// <summary>
         /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
         /// use an Initialize method
         /// </summary>
         public Class1()
         {
+        }
+        public int GetID()
+        {
+            CrestronConsole.PrintLine("Sample simplsharp callback");
+            return ID;
         }
     }
 }
