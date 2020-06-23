1b71f68ada495b5bd5bb8a293e4279b7a5246f17
Art Navsegda <artnavsegda@gmail.com>
2020-06-23 13:53:48 +0300

    hsv to rgb

--- hsvtorgb/HSVtoRGB.cs
+++ hsvtorgb/HSVtoRGB.cs
@@ -26,6 +26,14 @@ namespace hsvtorgb
             this.CalculateHSV();
         }
 
+        public void SetHSV(ushort hue, ushort saturation, ushort value)
+        {
+            this.Hue = hue;
+            this.Saturation = saturation;
+            this.Value = value;
+            this.CalculateRGB();
+        }
+
         public void CalculateHSV()
         {
             double red = (double) this.Red;
@@ -50,6 +58,101 @@ namespace hsvtorgb
                 this.Saturation = this.Scale(n2);
                 this.Value = this.Scale(n1);
             }
+        }
+
+        public void CalculateRGB()
+        {
+            double h = (double)this.Hue;
+            double S = (double)this.Saturation / 100;
+            double V = (double)this.Value / 100;
+
+            double H = h;
+            while (H < 0) { H += 360; };
+            while (H >= 360) { H -= 360; };
+            double R, G, B;
+            if (V <= 0)
+            { R = G = B = 0; }
+            else if (S <= 0)
+            {
+                R = G = B = V;
+            }
+            else
+            {
+                double hf = H / 60.0;
+                int i = (int)Math.Floor(hf);
+                double f = hf - i;
+                double pv = V * (1 - S);
+                double qv = V * (1 - S * f);
+                double tv = V * (1 - S * (1 - f));
+                switch (i)
+                {
+
+                    // Red is the dominant color
+
+                    case 0:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+
+                    // Green is the dominant color
+
+                    case 1:
+                        R = qv;
+                        G = V;
+                        B = pv;
+                        break;
+                    case 2:
+                        R = pv;
+                        G = V;
+                        B = tv;
+                        break;
+
+                    // Blue is the dominant color
+
+                    case 3:
+                        R = pv;
+                        G = qv;
+                        B = V;
+                        break;
+                    case 4:
+                        R = tv;
+                        G = pv;
+                        B = V;
+                        break;
+
+                    // Red is the dominant color
+
+                    case 5:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // Just in case we overshoot on our math by a little, we put these here. Since its a switch it won't slow us down at all to put these here.
+
+                    case 6:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+                    case -1:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
+
+                    // The color is not defined, we should throw an error.
+
+                    default:
+                        //LFATAL("i Value error in Pixel conversion, Value is %d", i);
+                        R = G = B = V; // Just pretend its black/white
+                        break;
+                }
+            }
+            this.Red = (ushort)(R * 65535);
+            this.Green = (ushort)(G * 65535);
+            this.Blue = (ushort)(B * 65535);
         }
 
         private double ScaleRBG(double value)
