919b022a3a72550e7fcae723027a6bf50fc94f99
Art Navsegda <artnavsegda@gmail.com>
2020-06-26 01:37:35 +0300

    helper class

---
+++ Helper/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Helper")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helper")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
